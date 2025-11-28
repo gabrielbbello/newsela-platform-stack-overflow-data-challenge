3b234347a2f8f14b306e3febf183f9ae34cf6082
Gabriel Bello <gabrielbbello@gmail.com>
2025-11-28 16:44:52 -0300

    feat: snapshot raw bigquery ddls

---
+++ src/ddl/users.sql
@@ -0,0 +1,17 @@
+CREATE TABLE `bigquery-public-data.stackoverflow.users`
+(
+  id INT64,
+  display_name STRING,
+  about_me STRING,
+  age STRING,
+  creation_date TIMESTAMP,
+  last_access_date TIMESTAMP,
+  location STRING,
+  reputation INT64,
+  up_votes INT64,
+  down_votes INT64,
+  views INT64,
+  profile_image_url STRING,
+  website_url STRING
+);
+
